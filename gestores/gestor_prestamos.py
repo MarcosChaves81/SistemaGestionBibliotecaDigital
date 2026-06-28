6055663ca5e671efcaa169763e50c56bf2deed62
santiee <santicalina30@gmail.com>
2026-06-28 17:38:00 -0300

    feat(gestor_prestamos: registrar préstamo con validaciones

--- gestores/gestor_prestamos.py
+++ gestores/gestor_prestamos.py
@@ -14,3 +14,17 @@ class GestionPrestamos:
             if prestamo.libro.isbn == isbn and prestamo.fecha_devolucion is None:
                 return prestamo
         return None
+
+   @anunciar_accion("registró")
+    def registrar_prestamo(self, admin, dni, isbn):
+        usuario = self._gestor_usuarios.buscar_por_dni(dni)
+        if not usuario:
+            raise ValueError(f"No se encontró un usuario con DNI {dni}")
+        libro = self._gestor_libros.buscar_por_isbn(isbn)
+        if not libro:
+            raise ValueError(f"No se encontró un libro con ISBN {isbn}")
+        if self.buscar_prestamo_activo(isbn):
+            raise ValueError("El libro ya tiene un préstamo activo")
+        prestamo = Prestamo(usuario, libro)
+        self._prestamos.append(prestamo)
+        
