1372b05bbf65701d99ad453594c26d95a2ae3365
MarcosChaves81 <marcosd.a.chaves3@gmail.com>
2026-06-28 22:05:01 -0300

    agregar diagrama UML en carpeta diagrama/uml

--- modelos/entidad_base.py
+++ modelos/entidad_base.py
@@ -2,5 +2,5 @@ from metaclases.meta_entidad import MetaEntidad
 
 
 class EntidadBase(metaclass=MetaEntidad):
-    """Clase base para las entidades que utilizan MetaEntidad."""
+    """Clase base para entidades controladas por MetaEntidad."""
     pass
